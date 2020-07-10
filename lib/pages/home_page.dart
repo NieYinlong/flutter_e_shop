a6022670bb955857c57eddc5d11bf7557d45ae99
聂银龙 <>
2020-07-10 14:56:35 +0800

    加广告条

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import '../config/service_method.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:html';
 
 class HomePage extends StatefulWidget {
   @override
@@ -31,13 +32,16 @@ class _HomePageState extends State<HomePage> {
             case ConnectionState.done:
               if (snapshot.hasError) return Text('Error: ${snapshot.error}', style: TextStyle(fontSize: ScreenUtil().setSp(32)),);
               var data = snapshot.data['data'];
+
               return Column(
                 children: <Widget>[
                   SwiperWidget(dataList: data['banners']),
                   NavCategory(categoryList: data['nav_categories'],),
+                  BannerAd(),
                   Text('待开发')
                 ],
               );
+
             default:
               return null;
           }
@@ -59,6 +63,11 @@ class SwiperWidget extends StatelessWidget {
     return Container(
       height: ScreenUtil().setHeight(222),
       child: Swiper(
+        onTap: (index) {
+          String linkUrl = dataList[index]['link_url'];
+          print('点击了${index} ${linkUrl}');
+          html.window.open();
+        },
         itemBuilder: (BuildContext context, int index) {
           var model = dataList[index];
           return Image.network(model['image']);
@@ -71,15 +80,18 @@ class SwiperWidget extends StatelessWidget {
   }
 }
 
+// 商品分类导航
 class NavCategory extends StatelessWidget {
 
   final List categoryList;
   NavCategory({Key key, this.categoryList}):super(key: key);
 
   Widget _grideViewItem(BuildContext context, item) {
+    // InkWell 有点击效果的小容器组件
     return InkWell(
       onTap: () {
         print('点击了 ${item['title']} url=${item['link_url']}');
+
       },
       child: Column(
         children: <Widget>[
@@ -93,11 +105,13 @@ class NavCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: ScreenUtil().setHeight(310),
+      //decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.pink)),
+      height: ScreenUtil().setHeight(260),
       padding: EdgeInsets.all(3),
       child: GridView.count(
         crossAxisCount: 5,
         padding: EdgeInsets.all(4),
+        physics: NeverScrollableScrollPhysics(), // 禁止滚动
         children: categoryList.map((item) {
           return _grideViewItem(context, item);
         }).toList(),
@@ -106,5 +120,15 @@ class NavCategory extends StatelessWidget {
   }
 }
 
+// 广告条
+class BannerAd extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: Image.network('https://gw.alicdn.com/imgextra/i1/1632892/O1CN01c1GKev1XEa1uzTYCL_!!1632892-0-lubanu.jpg'),
+    );
+  }
+}
 
 
